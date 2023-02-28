5e9caee327d71a9372523174423a8f04ee22faa5
stayingcozy <cody.colclough@gmail.com>
2023-02-28 15:43:47 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import MyButton from '@/components/MyButton'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps}></Component>
+      <MyButton />
+      <MyButton />
+
+    </>
+  );
 }
