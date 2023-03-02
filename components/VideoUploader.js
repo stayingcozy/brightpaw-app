3796a6129d333a42e8d4306aeaf5a7f07a489875
stayingcozy <cody.colclough@gmail.com>
2023-03-01 20:38:20 -0500

    got err handling upload section working

--- components/VideoUploader.js
+++ components/VideoUploader.js
@@ -29,65 +29,63 @@ export default function VideoUploader({ downloadURL, setDownloadURL }) {
     // Starts the upload
     const uploadTask  = uploadBytesResumable(storageRef,file,metadata);
 
-    // Listen to updates to upload task
-    uploadTask.on('state_changed', (snapshot) => {
-      const pct = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-      setProgress(pct);
+    // // Listen to updates to upload task
+    // uploadTask.on('state_changed', (snapshot) => {
+    //   const pct = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+    //   setProgress(pct);
 
-      // Get downloadURL AFTER task resolves - by using then (Note: this is not a native Promise) 
-      uploadTask
-      .then((d) => getDownloadURL(storageRef))
-      .then((url) => {
-        setDownloadURL(url);
-        setUploading(false);
-      });
-    });
-    };
-
-    // // Listen for state changes, errors, and completion of the upload.
-    // uploadTask.on('state_changed',
-    // (snapshot) => {
-    //   // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    //   const pct = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //   setProgress(pct)
-    //   console.log('Upload is ' + pct + '% done');
-    //   switch (snapshot.state) {
-    //     case 'paused':
-    //       console.log('Upload is paused');
-    //       break;
-    //     case 'running':
-    //       console.log('Upload is running');
-    //       break;
-    //   }
-    // }, 
-    // (error) => {
-    //   // A full list of error codes is available at
-    //   // https://firebase.google.com/docs/storage/web/handle-errors
-    //   switch (error.code) {
-    //     case 'storage/unauthorized':
-    //       // User doesn't have permission to access the object
-    //       break;
-    //     case 'storage/canceled':
-    //       // User canceled the upload
-    //       break;
-
-    //     // ...
-
-    //     case 'storage/unknown':
-    //       // Unknown error occurred, inspect error.serverResponse
-    //       break;
-    //   }
-    // }, 
-    // () => {
-    //   // Upload completed successfully, now we can get the download URL
-    //   getDownloadURL(uploadTask.snapshot.vidRef).then((downloadURL) => {
-    //     console.log('File available at', downloadURL);
-    //     setDownloadURL(downloadURL);
+    //   // Get downloadURL AFTER task resolves - by using then (Note: this is not a native Promise) 
+    //   uploadTask
+    //   .then((d) => getDownloadURL(storageRef))
+    //   .then((url) => {
+    //     setDownloadURL(url);
     //     setUploading(false);
     //   });
-    // }
-    // );
-  // };
+    // });
+    // };
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed', (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const pct = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+      setProgress(pct)
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    }, 
+    (error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    }, 
+    () => {
+      // Upload completed successfully, now we can get the download URL
+      getDownloadURL(storageRef).then((URL) => {
+        console.log('File available at', URL);
+        setDownloadURL(URL);
+        setUploading(false);
+      });
+    }
+    );
+  };
 
   return (
     <div className="box">
