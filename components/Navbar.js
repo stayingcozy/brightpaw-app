a13d9182cad6f8777ceb2aaee1636555d92288c4
stayingcozy <cody.colclough@gmail.com>
2023-06-12 10:36:01 -0400

    fix firebase, recap AND stripe navbar start

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '@/lib/context';
 
 // top navbar
-export default function Navbar() {
+export default function Navbar(context) {
     
     const {user,username} = useContext(UserContext)
+    const router = useRouter()
+
 
     return (
         <nav className="navbar">
@@ -13,11 +16,43 @@ export default function Navbar() {
                 {/* user is signed-in and has username */}
                 {username && (
                  <>
-                 <li>
-                    <Link href={`/${username}`}>
-                        <img src={user?.photoURL} />
-                    </Link>
-                 </li>
+                    <li>
+                        <Link href={`${username}/checkout`}>
+                            <span aria-label="emoji" role="img">
+                            🛒
+                            </span>{' '}
+                            Checkout
+                        </Link> 
+                    </li>
+                    <li>
+                        <Link href={`${username}/payments`}>
+                            <span aria-label="emoji" role="img">
+                            💸
+                            </span>{' '}
+                            Payments
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={`${username}/customers`}>
+                            <span aria-label="emoji" role="img">
+                            🧑🏿‍🤝‍🧑🏻
+                            </span>{' '}
+                            Customers
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={`${username}/subscriptions`}>
+                            <span aria-label="emoji" role="img">
+                            🔄
+                            </span>{' '}
+                            Subscriptions
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={`/${username}`}>
+                            <img src={user?.photoURL} />
+                        </Link>
+                    </li>
                  </>
                 )}
                 {/* user is not signed-in and has not created username */}
